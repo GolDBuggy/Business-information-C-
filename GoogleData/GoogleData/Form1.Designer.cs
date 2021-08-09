f48134816586064ca89b674fdf065ed98ac92623
GolDBuggy <87481140+GolDBuggy@users.noreply.github.com>
2021-08-09 18:08:45 +0300

    Bussiness İnformation
    This project we can pull business information with Google.Which company you need address or phone number,write and click 'Ara' button.
    İf you need save the info you can use the   button 'Excele Kaydet' and saves the info by excel.

---
+++ GoogleData/GoogleData/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace GoogleData
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Excel Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Yaz ve Ara";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(134, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(292, 212);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 312);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ GoogleData/GoogleData/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace GoogleData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+        }
+       
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int adet = listBox1.Items.Count;
+            if (listBox1.Items.Count > 0)
+            {
+                Random rastgele = new Random();
+                int sayi = rastgele.Next();
+
+                string dosyayolu;
+
+                dosyayolu = "D:\\" + sayi.ToString() + ".xlsx";
+                Excel.Application excelapp = new Excel.Application();
+                excelapp.Visible = true;
+
+                object Missing = Type.Missing;
+
+                Workbook calismakitabi = excelapp.Workbooks.Add(Missing);
+                
+                Worksheet sheet1 = (Worksheet)calismakitabi.Sheets[1];
+                int sutun = 1;
+                int satir = 1;
+                for (int i = 0; i < adet; i++)
+                {
+                    for (int j = 0; j < 1; j++)
+                    {
+                        Range myrange1 = (Range)sheet1.Cells[satir + i, sutun + j];
+                        myrange1.Value2 = listBox1.Items[i] == null ? "" : listBox1.Items[i];
+                        myrange1.Select();
+
+                    }
+                }
+
+               
+                
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void yazAra()
+        {
+            
+            
+            try
+            {
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                options.AddExcludedArgument("enable-automation");
+                options.AddArgument("--headless");
+                IWebDriver driver = new ChromeDriver(service,options);
+                driver.Navigate().GoToUrl("https://www.google.com/");
+                Thread.Sleep(5000);
+                IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+                element.SendKeys(textBox1.Text);
+                Thread.Sleep(2000);
+                driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[2]/center/input[1]")).Click();
+                Thread.Sleep(2000);
+
+                for (int i = 1; i < 2; i++)
+                {
+                    try
+                    {
+                        string span = driver.FindElement(By.ClassName("LrzXr")).Text;
+                        string ad = driver.FindElement(By.ClassName("qrShPb")).Text;
+                        string tel = driver.FindElement(By.ClassName("zdqRlf")).Text;
+                        string hour = driver.FindElement(By.ClassName("JjSWRd")).Text;
+
+
+                        listBox1.Items.Add("Firma Adı: " + ad + "  Adresi: " + span + " Telefon:" + tel + "Çalışma Durumu: "+hour );
+
+                    }
+                    catch (Exception)
+                    {
+
+                        string span = driver.FindElement(By.ClassName("LrzXr")).Text;
+                        string ad = driver.FindElement(By.ClassName("qrShPb")).Text;
+                        string tel = driver.FindElement(By.ClassName("zdqRlf")).Text;
+
+                        listBox1.Items.Add("Firma Adı: " + ad + "  Adresi: " + span + " Telefon:" + tel );
+                    }
+
+                   
+                    
+
+
+
+
+
+
+
+
+                }
+                driver.Close();
+
+
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Çalışma Saati Belli Değil!");
+                
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(yazAra);
+            th.Start();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
